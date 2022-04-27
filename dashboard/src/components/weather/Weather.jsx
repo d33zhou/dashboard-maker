4774968970079401baf86fd8aafde549b50534a0
David Zhou <d33zhou@outlook.com>
2022-04-26 23:50:43 -0400

    set up API fetch logic

--- dashboard/src/components/weather/Weather.jsx
+++ dashboard/src/components/weather/Weather.jsx
@@ -1,48 +1,75 @@
 import { TextField, Box, Paper, Typography } from '@mui/material';
 import Moment from 'react-moment';
-import {useEffect} from 'react'
+import {useState, useEffect} from 'react'
+
+const OPENWEATHER_API_KEY = "";
+const API_URL_BASE = `https://api.openweathermap.org/data/2.5/`
 
 const Weather = props => {
   const current = new Date();
+
+  const [weather, setWeather] = useState({
+    temperature: 0,
+    feels_like: 0,
+    main: "Sunny",
+    wind: 0
+  });
+
+  const search = event => {
+    if (event.key === "Enter") {
+      fetch(`${API_URL_BASE}weather?q=Toronto&units=metric&appid=${OPENWEATHER_API_KEY}`)
+        .then(res => res.json())
+        .then(result => console.log(result));
+    }
+  };
 
   return (
     <Paper
       sx={{
         height: '600px',
-        width: '300px'
+        width: '300px',
       }}
       elevation={4}
     >
-      {/* Location Search */}
-      <TextField
+      <Box
         sx={{
-          width:'100%'
+          height: '100%',
+          width: '100%',
         }}
-        label="Search for location"
-        variant="filled"
-      />
+      >
 
-      {/* Date/Time */}
-      <Typography>
-        <Moment format="MMMM DD, YYYY">
-          {current}
-        </Moment>
-      </Typography>
+        {/* Location Search */}
+        <TextField
+          sx={{
+            width:'100%'
+          }}
+          label="Search for location"
+          variant="filled"
+        />
 
-      {/* Temperature */}
-      <Typography>
-        15°C
-      </Typography>
-      
-      {/* Forecast */}
-      <Typography>
-        Sunny
-      </Typography>
+        {/* Date/Time */}
+        <Typography>
+          <Moment format="MMMM DD, YYYY">
+            {current}
+          </Moment>
+        </Typography>
 
-      {/* Location */}
-      <Typography>
-        Toronto, Ontario
-      </Typography>
+        {/* Temperature */}
+        <Typography>
+          {weather.temperature}°C
+        </Typography>
+        
+        {/* Forecast */}
+        <Typography>
+          {weather.main}
+        </Typography>
+
+        {/* Location */}
+        <Typography>
+          Toronto, Ontario
+        </Typography>
+
+      </Box>
     </Paper>
   );
 };
