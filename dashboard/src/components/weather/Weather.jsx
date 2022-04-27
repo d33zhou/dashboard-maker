2c0abaf772105c51d2260105a348f21d9316c476
David Zhou <d33zhou@outlook.com>
2022-04-27 00:39:21 -0400

    update query logic and search handling

--- dashboard/src/components/weather/Weather.jsx
+++ dashboard/src/components/weather/Weather.jsx
@@ -2,24 +2,52 @@ import { TextField, Box, Paper, Typography } from '@mui/material';
 import Moment from 'react-moment';
 import {useState, useEffect} from 'react'
 
-const OPENWEATHER_API_KEY = "";
+// API call limit --> 60/min, 1 million/month
+const OPENWEATHER_API_KEY = ""; // REMOVE FROM PUBLIC VIEW
 const API_URL_BASE = `https://api.openweathermap.org/data/2.5/`
 
 const Weather = props => {
   const current = new Date();
 
   const [weather, setWeather] = useState({
-    temperature: 0,
-    feels_like: 0,
-    main: "Sunny",
-    wind: 0
+    main: {
+      feels_like: 0,
+      humidity: 0,
+      pressure: 0,
+      temp: 0,
+      temp_max: 0,
+      temp_min: 0
+    },
+    name: "Toronto",
+    sys: {
+      country: "CA",
+      sunrise: 0,
+      sunset: 0
+    },
+    visibility: 0,
+    weather: [
+      {
+        description: "clear",
+        main: "Clear"
+      }
+    ],
+    wind: {
+      deg: 0,
+      gust: 0,
+      speed: 0
+    },
   });
+
+  const [query, setQuery] = useState('');
 
   const search = event => {
     if (event.key === "Enter") {
-      fetch(`${API_URL_BASE}weather?q=Toronto&units=metric&appid=${OPENWEATHER_API_KEY}`)
+      fetch(`${API_URL_BASE}weather?q=${query}&units=metric&appid=${OPENWEATHER_API_KEY}`)
         .then(res => res.json())
-        .then(result => console.log(result));
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+        });
     }
   };
 
@@ -45,6 +73,9 @@ const Weather = props => {
           }}
           label="Search for location"
           variant="filled"
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
         />
 
         {/* Date/Time */}
@@ -56,17 +87,17 @@ const Weather = props => {
 
         {/* Temperature */}
         <Typography>
-          {weather.temperature}°C
+          {weather.main.temp}°C
         </Typography>
         
         {/* Forecast */}
         <Typography>
-          {weather.main}
+          {weather.weather[0].main}
         </Typography>
 
         {/* Location */}
         <Typography>
-          Toronto, Ontario
+          {weather.name}
         </Typography>
 
       </Box>
